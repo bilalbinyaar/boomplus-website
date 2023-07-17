f62de3d6c59e8be04b1865372e5ff613e6491754
Bilal Y <bilal.y@boomdc.local>
2023-07-17 16:33:19 +0300

    Normalizing Css

--- src/components/salt/SaltBanner.js
+++ src/components/salt/SaltBanner.js
@@ -2,11 +2,28 @@ import React from 'react'
 
 const SaltBanner = () => {
   return (
-    <div className='salt-banner'>
+    <div className='salt-banner-main'>
+        <div className='salt-banner'>
         <div className='container'>
+            <div className='salt-banner-content'>
+                <div className='salt-banner-content-left'>
+                    <div className='salt-banner-content-left-top'>
+                        <h2>FIRST TIME TO IMPORT?</h2>
+                        <h3>We can help</h3>
+                    </div>
+                    <div className='salt-banner-content-left-bottom'>
+                        <h1>Coarse to fine, the whole Range's available</h1>
+                    </div>
+                </div>
+                <div className='salt-banner-content-right'>
 
+                    This is Right
+                </div>
+            </div>
         </div>
     </div>
+    </div>
+    
   )
 }
 
