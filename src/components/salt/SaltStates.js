a5c460852ed0e0b79342ce3c70b74f65d7218ee3
Bilal Y <bilal.y@boomdc.local>
2023-07-19 15:25:15 +0300

    Pushing Code for Stats Counter

--- src/components/salt/SaltStates.js
+++ src/components/salt/SaltStates.js
@@ -4,10 +4,13 @@ const SaltStates = () => {
   return (
     <div className='salt-stats'>
         <div className='container'>
+            <div className='section-title'>
+                <h2>Statistics</h2>
+            </div>
             <div className='salt-stats-wrapper'>
                 <div className='stats-counter'>
-                    counter
-                </div>
+                    
+                                    </div>
                 <div className='stats-content'>
                     content
                 </div>
